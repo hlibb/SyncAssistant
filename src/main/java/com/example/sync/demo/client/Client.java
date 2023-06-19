d44831af68b83304d805999ba85e81227501a580
Astashev Hlib <hlib.astashev@exceet.de>
2023-06-19 16:42:24 +0200

    edited client

--- src/main/java/com/example/sync/demo/client/Client.java
+++ src/main/java/com/example/sync/demo/client/Client.java
@@ -11,11 +11,13 @@ public class Client {
     @SneakyThrows
     public static void main(String[] args) {
         //System.out.println(InetAddress.getLocalHost().getHostAddress()); ws://127.0.1.1:8080/websocket
-        try (var socket = new Socket("127.0.1.1", 8899)){
+        try (var socket = new Socket("95.223.45.239", 8899)){
             var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             var message = "Hello \n";
             writer.write(message);
             writer.flush();
+        } catch (Exception e) {
+            System.out.println("Client error");
         }
     }
 }
